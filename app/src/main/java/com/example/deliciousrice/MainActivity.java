bd575c3b4b8443ad745c2516f062e8b92bdfae6b
ngocbattu <you@example.comtranthengoc02072002@gmail.com>
2022-09-23 22:39:16 +0700

    tran the ngoc update

--- app/src/main/java/com/example/deliciousrice/MainActivity.java
+++ app/src/main/java/com/example/deliciousrice/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //nguyen duc thinh 2002
         //van yeu hoa
+        // tran the ngoc
     }
 }
