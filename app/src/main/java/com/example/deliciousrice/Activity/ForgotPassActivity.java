5a9bb662a1f8fa07bf3a52d7672ca1233a61984b
NDThinhahihi <94670286+NDThinhahihi@users.noreply.github.com>
2022-09-25 02:19:13 +0700

    add activity

---
+++ app/src/main/java/com/example/deliciousrice/Activity/ForgotPassActivity.java
@@ -0,0 +1,16 @@
+package com.example.deliciousrice.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.deliciousrice.R;
+
+public class ForgotPassActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_pass);
+    }
+}
---
+++ app/src/main/java/com/example/deliciousrice/Activity/HelloScreenActivity.java
@@ -0,0 +1,16 @@
+package com.example.deliciousrice.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.deliciousrice.R;
+
+public class HelloScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_screen);
+    }
+}
