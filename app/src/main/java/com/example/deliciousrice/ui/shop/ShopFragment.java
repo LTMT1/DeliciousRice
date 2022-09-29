75afe716dcba23ba91bd84a7ac6ed7289b8939d9
HuuPhuc0672 <nguyenhuuphuc672002@gmail.com>
2022-09-29 23:42:34 +0700

    Phuc

--- app/src/main/java/com/example/deliciousrice/ui/shop/ShopFragment.java
+++ app/src/main/java/com/example/deliciousrice/ui/shop/ShopFragment.java
@@ -7,12 +7,22 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
+import com.example.deliciousrice.R;
 import com.example.deliciousrice.databinding.FragmentShopBinding;
 
+import java.util.ArrayList;
+
 public class ShopFragment extends Fragment {
+    private ImageSlider imgSilde;
+
+
 
     private FragmentShopBinding binding;
 
@@ -23,10 +33,20 @@ public class ShopFragment extends Fragment {
 
         binding = FragmentShopBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        imgSilde = view.findViewById(R.id.imgSilde);
+        ArrayList<SlideModel> models=new ArrayList<>();
+        models.add(new SlideModel(R.drawable.img_silde1,null));
+        models.add(new SlideModel(R.drawable.img_silde2,null));
+        models.add(new SlideModel(R.drawable.img_silde3,null));
+        models.add(new SlideModel(R.drawable.img_silde4,null));
+        models.add(new SlideModel(R.drawable.img_silde5,null));
+        imgSilde.setImageList(models, ScaleTypes.CENTER_CROP);
     }
 
     @Override
