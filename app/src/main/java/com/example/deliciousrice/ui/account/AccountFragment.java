aa25c6263551dc4da8c9a59dbbfea0e730cd1620
Thangph13623 <halloeveryone2002@gmail.com>
2022-11-10 21:44:07 +0700

    nguyenthang13623

--- app/src/main/java/com/example/deliciousrice/ui/account/AccountFragment.java
+++ app/src/main/java/com/example/deliciousrice/ui/account/AccountFragment.java
@@ -5,11 +5,9 @@ import static android.content.Context.MODE_PRIVATE;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -18,14 +16,10 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.bumptech.glide.Glide;
 import com.example.deliciousrice.Activity.ChangePassActivity;
 import com.example.deliciousrice.Activity.LoginActivity;
-import com.example.deliciousrice.Activity.LoginFaGoActivity;
-import com.example.deliciousrice.Activity.RegisterActivity;
-import com.example.deliciousrice.MainActivity;
 import com.example.deliciousrice.MainActivity2;
 import com.example.deliciousrice.R;
 import com.example.deliciousrice.databinding.FragmentAccountBinding;
@@ -35,11 +29,6 @@ import com.example.deliciousrice.ui.account.Activity.InformationActivity;
 import com.example.deliciousrice.ui.account.Activity.PolicyActivity;
 import com.example.deliciousrice.ui.account.Activity.ReceiptActivity;
 import com.example.deliciousrice.ui.account.Activity.SettingActivity;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.tasks.OnSuccessListener;
-
-import java.io.Serializable;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -55,7 +44,7 @@ public class AccountFragment extends Fragment {
     private ConstraintLayout clPolicy;
     private ConstraintLayout clSetting;
     private ConstraintLayout clDoipass;
-    private Button tvDangXuat;
+    private TextView tvDangXuat;
     private MainActivity2 main;
     private FragmentAccountBinding binding;
 
